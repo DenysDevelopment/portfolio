f1e5d150d7c2675d3627537ce35ffe767a995303
DenysDevelopment <dinis_05@ukr.net>
2021-12-25 09:58:00 +0200

    target _blank

--- src/components/Work/Item.tsx
+++ src/components/Work/Item.tsx
@@ -16,7 +16,7 @@ export const Item = ({ name, link, desk, urlImg }: ItemPropsType) => {
 			<div className='card-work__body'>
 				<h2 className='card-work__title'>{name}</h2>
 				<p className='card-work__description'>{desk}</p>
-				<a href={link} className='card-work__btn' title='Перейти на сайт'>
+				<a href={link} target='_blank' className='card-work__btn' title='Перейти на сайт'>
 					Перейти на сайт
 				</a>
 			</div>
